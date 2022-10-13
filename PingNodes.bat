632664424855e1992f8e1527e5bbfb2cbbdbfcbc
rhass <robhass@hotmail.com>
2022-10-12 22:53:49 -0500

    initial loading of files

---
+++ PingNodes.bat
@@ -0,0 +1,6 @@
+start "hallow_1" /low ping hallow_1 -t
+start "hallow_2" /low ping hallow_2 -t
+start "hallow_3" /low ping hallow_3 -t
+start "hallow_4" /low ping hallow_4 -t
+start "hallow_5" /low ping hallow_5 -t
+start "hallow_6" /low ping hallow_6 -t
